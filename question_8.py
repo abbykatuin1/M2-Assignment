1b70342a2df57107a49f93e0791eb857f6f75e08
Abby Katuin <u1515892@utah.edu>
2025-08-25 14:15:33 -0600

    Create question_8.py

---
+++ question_8.py
@@ -0,0 +1,8 @@
+#Susan's weight is 60 kilograms
+#Susan's height is 1.58 meters.
+#calculate amd print susan's body max index as float 
+#BMI = weight in kilograms / square of height in meteres
+weight = 60
+height = 1.58
+bmi = weight/(height**2)
+print(bmi)
